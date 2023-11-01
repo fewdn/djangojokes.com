e71f7eae81f1f862bcbed0a6894725029971278e
Owen <username@example.com>
2023-11-01 14:11:43 -0700

    create project based static files favicon and logo

--- djangojokes/settings.py
+++ djangojokes/settings.py
@@ -123,6 +123,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',   # A project-based static files directory
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
