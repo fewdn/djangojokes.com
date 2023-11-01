3e307496a025a1c74909062517a5ce7a144fd495
Owen <username@example.com>
2023-11-01 13:00:35 -0700

    Create common app with custom template tag

--- djangojokes/settings.py
+++ djangojokes/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     # local apps - organized alphabetically by default
+    'common.apps.CommonConfig',
     'jokes.apps.JokesConfig', 
     'pages.apps.PagesConfig',
 ]
