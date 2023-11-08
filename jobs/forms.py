3b972df90e7a2131c758cb8bafe6cafaab991b35
Owen <username@example.com>
2023-11-08 12:04:37 -0800

    Modified application form to use widgets

--- jobs/forms.py
+++ jobs/forms.py
@@ -1,23 +1,18 @@
+from datetime import datetime
 from django import forms
 
+# REFERENCE: https://docs.djangoproject.com/en/4.2/ref/forms/fields/
 class JobApplicationForm(forms.Form):
-    first_name = forms.CharField()
-    last_name = forms.CharField()
-    email = forms.EmailField()
-    website = forms.URLField(required=False)
-
-    employment_choices = (
+    date = datetime.now()
+    year = date.year
+    next_year = year + 1
+    YEARS = [year, next_year]
+    EMPLOYMENT_TYPES = (
         ('None', '--Please Choose--'),
         ('ft', 'full-time'),
         ('pt', 'part-time'),
     )
-    employment_type = forms.ChoiceField(choices=employment_choices)
-
-    start_date = forms.DateField(
-        help_text='The earliest date you can start working'
-    )
-
-    day_choices = (
+    DAYS = (
         ('1', 'MON'),
         ('2', 'TUE'),
         ('3', 'WED'),
@@ -26,13 +21,51 @@ class JobApplicationForm(forms.Form):
         ('6', 'SAT'),
         ('7', 'SUN'),
     )
-    available_days = forms.MultipleChoiceField(
-        choices=day_choices,
-        help_text='Select all days that you can work.'
+
+    # Default widget for CharField is TextInput
+    first_name = forms.CharField(
+        # for single-named attributes use True. 'autofocus': True vs 'autofocus':'autofocus'
+        widget=forms.TextInput(attrs={'autofocus':True})  
+    )
+    last_name = forms.CharField()
+    email = forms.EmailField()
+
+    # Default widget for URLField is URLInput
+    website = forms.URLField(
+        required=False,
+        widget=forms.URLInput(
+            attrs={'placeholder':'https://wwww.example.com', 'size': '50'}
+        )
     )
 
-    desired_hourly_wage = forms.DecimalField(decimal_places=2)
-    cover_letter = forms.CharField()
+    employment_type = forms.ChoiceField(choices=EMPLOYMENT_TYPES)
+
+    start_date = forms.DateField(
+        widget=forms.SelectDateWidget(
+            years=YEARS,
+        ),
+        help_text='The earliest date you can start working'
+    )
+
+    # For single value attributes use 'checked':True, instead of "checked":"checked"
+    available_days = forms.TypedMultipleChoiceField(
+        choices=DAYS,
+        coerce=int,
+        help_text='Check all days that you can work.',
+        widget=forms.CheckboxSelectMultiple(
+            attrs={'checked': True}  
+        )
+    )
+
+    # Default widget for DecimalField is NumberInput
+    desired_hourly_wage = forms.DecimalField(
+        widget=forms.NumberInput(
+            attrs={'min': '10.00', 'max': '100.00', 'step': '.25'}
+        )
+    )
+    cover_letter = forms.CharField(
+        widget=forms.Textarea( attrs={'cols': '75', 'rows': '5'})
+    )
     confirmation = forms.BooleanField(
         label="I certify that the information I have provided is true."
     )
