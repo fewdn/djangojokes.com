156d408dc0525673eea1bb529ccc48b4632230a4
Owen <username@example.com>
2023-11-29 11:00:50 -0800

    Add messages to JokeDeleteView

--- jokes/views.py
+++ jokes/views.py
@@ -4,6 +4,7 @@ from django.views.generic import (
     CreateView, DeleteView, DetailView, ListView, UpdateView
 )
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib import messages
 from django.contrib.messages.views import SuccessMessageMixin
 from .models import Joke
 from .forms import JokeForm
@@ -23,9 +24,17 @@ class JokeDeleteView(UserPassesTestMixin, DeleteView):
     model = Joke
     success_url = reverse_lazy('jokes:list')
 
+    def delete(self, request, *args, **kwargs):
+        result = super().delete(request, *args, **kwargs)
+        return result
+
     def test_func(self):
         obj = self.get_object()
         return self.request.user == obj.user
+    
+    def form_valid(self, form):
+        messages.success(self.request, 'Joke deleted')
+        return super().form_valid(form)
     
 class JokeDetailView(DetailView):
     model = Joke
