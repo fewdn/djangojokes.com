94a9397d728b83dd6a25fc8765d9ce0a696b5211
Owen <username@example.com>
2023-12-03 22:50:26 -0800

    Add static storage for local development only

--- djangojokes/settings.py
+++ djangojokes/settings.py
@@ -225,6 +225,8 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# static files are local for development only
 if os.environ.get('ENVIRONMENT') != 'production':
     from .local_settings import *
 # DON'T PUT ANYTHING BELOW THIS
