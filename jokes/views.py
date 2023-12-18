57139c831c41dea957352dabdd64141c16fb5982
Owen <username@example.com>
2023-12-18 15:29:23 -0800

    Add pagination to joke ListView and template

--- jokes/views.py
+++ jokes/views.py
@@ -43,6 +43,7 @@ class JokeDetailView(DetailView):
     
 class JokeListView(ListView):
     model = Joke
+    paginate_by = 10
 
 class JokeUpdateView(SuccessMessageMixin, UserPassesTestMixin, UpdateView):
     model = Joke
