7e0f7e4238f743aaf7fb1e2f70fe6991e4d2cf1a
Owen <username@example.com>
2023-12-20 20:38:27 -0800

    Add QuerySet modifier for ListView to limit joke category tag and creator

--- jokes/views.py
+++ jokes/views.py
@@ -81,7 +81,6 @@ class JokeListView(ListView):
 
         return (order_fields, order_key, direction)
 
-    
     def get_order_fields(self):
         # Returns a dict mapping friendly names to field names and lookups.
         return {
@@ -92,6 +91,22 @@ class JokeListView(ListView):
             'updated': 'updated',
             'default_key': 'updated'
         }
+    
+    def get_queryset(self):
+        ordering = self.get_ordering()
+        qs = Joke.objects.all()
+
+        if 'slug' in self.kwargs: # Filter by category or tag
+            slug = self.kwargs['slug']
+            if '/category' in self.request.path_info:
+                qs = qs.filter(category__slug=slug)
+            if '/tag' in self.request.path_info:
+                qs = qs.filter(tags__slug=slug)
+        elif 'username' in self.kwargs: # Filter by joke creator
+            username = self.kwargs['username']
+            qs = qs.filter(user__username=username)
+
+        return qs.order_by(ordering)
 
 
 class JokeUpdateView(SuccessMessageMixin, UserPassesTestMixin, UpdateView):
